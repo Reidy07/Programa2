73ec2f9a8e8772f526e13fa7fe1d8f89e53d9dcd
Reidy07 <reidybrown1234@gmail.com>
2024-05-09 09:37:13 -0400

    Agregar archivos de proyecto.

---
+++ Programa2/Programa#2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Programa2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            double numero3;
+            double numero4;
+            double suma;
+            double producto;
+
+            try
+            {
+                Console.WriteLine("Ingresar el primer numero:");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar el segundo numer:");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar el tercer numero:");
+                numero3 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar el cuarto numero:");
+                numero4 = Convert.ToDouble(Console.ReadLine());
+
+                suma = numero1 + numero2;
+
+                producto = numero3 * numero4;
+
+                Console.WriteLine("La suma de los dos primeros numeros es:" + suma);
+                Console.WriteLine("El producto del tercer y cuarto numero es:" + producto);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"El problema se encuantra aqui {ex.Message}");
+            }
+        }
+    }
+}
